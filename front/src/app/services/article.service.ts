adb47db62d2e12c5240bd11557f40e588d9f6701
LAMBERT Quentin <Quentin.Lambert@solidaris.be>
2023-03-29 16:04:37 +0200

    dev: service article

---
+++ front/src/app/services/article.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Article } from '../interfaces/article';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ArticleService {
+  articles: Article[] = [
+    {
+      id: 'a1',
+      name: 'Tournevis',
+      price: 3.99,
+      qty: 100,
+    },
+    {
+      id: 'a2',
+      name: 'Pelle',
+      price: 5.99,
+      qty: 10,
+    },
+  ];
+  constructor() {}
+}
--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
 import { faRotateRight, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { Article } from '../interfaces/article';
+import { ArticleService } from '../services/article.service';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
   styleUrls: ['./stock.component.scss'],
 })
-export class StockComponent {
-  refreshIcon = faRotateRight;
+export class StockComponent implements OnDestroy {
   faPlus = faPlus;
+  refreshIcon = faRotateRight;
   trashIcon = faTrashAlt;
+
+  constructor(readonly articleService: ArticleService) {
+    console.log('articleService: ', articleService);
+    console.log('Stock component birth');
+  }
+
+  ngOnDestroy(): void {
+    console.log('Stock component disappear');
+  }
 }
