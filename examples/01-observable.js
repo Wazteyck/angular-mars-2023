1a628faa2526c1f2341e70128712c4746172d8e8
Quentin Lambert <lambert.quentin96@gmail.com>
2023-03-30 11:01:21 +0200

    dev: cancellation observable

--- examples/01-observable.js
+++ examples/01-observable.js
@@ -1,17 +1,37 @@
 const rxjs = require("rxjs");
 
-const { Observable } = rxjs;
+const { Observable, throwError } = rxjs;
 
+// Old version unused now
 const obs = new Observable((subscriber) => {
   subscriber.next(12);
   subscriber.next("Hello World!");
   subscriber.next({ a: "11" });
-  subscriber.error(new Error("Error happend"));
 
+  // Error or complete, not both
+  //subscriber.error(new Error("Error happend"));
+  // Not execute
   subscriber.complete();
 });
 
-obs.subscribe({
+const obsError = throwError(new Error("Error happend"));
+
+const obs2 = new Observable((subscriber) => {
+  subscriber.next(0);
+
+  const timer = setTimeout(() => {
+    subscriber.next(1);
+    subscriber.complete();
+    console.log("end setTimeOut()");
+  }, 1000);
+
+  return () => {
+    console.log("unsubstribing");
+    clearTimeout(timer);
+  };
+});
+
+const subscription = obs2.subscribe({
   next: (x) => {
     console.log(x);
   },
@@ -22,3 +42,7 @@ obs.subscribe({
     console.log("err: " + err.stack);
   },
 });
+
+setTimeout(() => {
+  subscription.unsubscribe();
+}, 500);
